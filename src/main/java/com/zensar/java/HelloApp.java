5e44162468e2e86dcb2e4437e98b83f161ed3642
dharamp1 <dharmaraj.pawale@gmail.com>
2019-08-14 23:10:50 +0530

    Initial Commit Added Java Project

---
+++ src/main/java/com/zensar/java/HelloApp.java
@@ -0,0 +1,13 @@
+package com.zensar.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
